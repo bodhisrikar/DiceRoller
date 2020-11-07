5bc421b31f8657a2345f4d0e4fea9c0ecd03383b
bodhisrikar <tsrikar187@gmail.com>
2020-11-07 08:08:45 -0500

    setting root view using view binding

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainActivityBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+        val root = mainActivityBinding.root
+        setContentView(root)
+
+        mainActivityBinding.rollButton.text = "Let's Roll"
     }
 }
